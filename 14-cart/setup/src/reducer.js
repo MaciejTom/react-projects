8740a113420552027622910d08bb2d257e144d56
Maciej Tomaszewicz <1994tomaszewicz@gmail.com>
2021-11-22 13:16:53 +0100

    Searching functionality added

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -11,62 +11,44 @@ const reducer = (state, action) => {
           return el.id !== action.payload;
         }),
       };
-      case "TOGGLE_AMOUNT":
-        const tempCart = state.cart.map((cartItem) => {
+    case "TOGGLE_AMOUNT":
+      const tempCart = state.cart
+        .map((cartItem) => {
           if (cartItem.id == action.payload.id) {
-            return { ...cartItem, amount: action.payload.type == "increase" ? cartItem.amount + 1 : cartItem.amount - 1};
+            return {
+              ...cartItem,
+              amount:
+                action.payload.type == "increase"
+                  ? cartItem.amount + 1
+                  : cartItem.amount - 1,
+            };
           }
           return cartItem;
-        }).filter((el) => el.amount > 0);
-        return { ...state, cart: tempCart };
-
-
-    // case "INCREASE":
-    //   const tempCart = state.cart.map((cartItem) => {
-    //     if (cartItem.id == action.payload) {
-    //       return { ...cartItem, amount: cartItem.amount + 1 };
-    //     }
-    //     return cartItem;
-    //   });
-    //   return { ...state, cart: tempCart };
-    // case "DECREASE":
-    //   const tempCart2 = state.cart
-    //     .map((cartItem) => {
-    //       if (cartItem.id == action.payload) {
-    //         if (cartItem == 0) {
-    //         }
-    //         return { ...cartItem, amount: cartItem.amount - 1 };
-    //       }
-    //       return cartItem;
-    //     })
-    //     .filter((el) => el.amount > 0);
-    //   return { ...state, cart: tempCart2 };
-
-
-
+        })
+        .filter((el) => el.amount > 0);
+      return { ...state, cart: tempCart };
 
     case "GET_TOTALS":
-      let { total, amount } = state.cart.reduce((cartTotal, cartItem) => {
-        const { price, amount } = cartItem
-        cartTotal.amount += amount
-        cartTotal.total += (amount * price)
-        return cartTotal
-      }, {
-        total: 0,
-        amount: 0,
-      });
-      total = total.toFixed(2); 
-    
+      let { total, amount } = state.cart.reduce(
+        (cartTotal, cartItem) => {
+          const { price, amount } = cartItem;
+          cartTotal.amount += amount;
+          cartTotal.total += amount * price;
+          return cartTotal;
+        },
+        {
+          total: 0,
+          amount: 0,
+        }
+      );
+      total = total.toFixed(2);
 
-    return {...state, amount, total};
+      return { ...state, amount, total };
 
     case "LOADING":
-      
-    return {...state, loading: true}
+      return { ...state, loading: true };
     case "DISPLAY_ITEM":
-      
-    return {...state, cart: action.payload, loading: false}
-
+      return { ...state, cart: action.payload, loading: false };
 
     default:
       new Error("REDUCER: ŻADNA Z PODANYCH AKCJI");
